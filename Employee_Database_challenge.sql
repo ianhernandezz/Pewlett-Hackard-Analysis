17669e9b9e9999ef60ef45280651a80addf9c2f9
Ian Hernandez <92615504+ianhernandezz@users.noreply.github.com>
2021-12-09 18:29:56 -0500

    commit

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -20,3 +20,30 @@ RT.title
 INTO unique_titles
 FROM retirement_titles AS RT
 ORDER BY RT.emp_no, RT.to_date DESC;
+
+-- 
+SELECT COUNT(UT.title), UT. title
+INTO retiring_titles
+FROM unique_titles AS UT
+GROUP BY UT.title
+ORDER BY COUNT DESC;
+
+-- END DELIVERABLE 1
+
+-- DELIVERABLE 2 
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibilty 
+FROM employees AS e
+INNER JOIN dept_emp as de
+	ON(e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+	ON(e.emp_no = ti.emp_no)
+	
+
