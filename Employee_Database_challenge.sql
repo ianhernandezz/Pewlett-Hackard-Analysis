4aba9a0c76137052dc3a35219308e6f21afa7970
Ian Hernandez <92615504+ianhernandezz@users.noreply.github.com>
2021-12-09 19:16:17 -0500

    commit

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -46,4 +46,22 @@ INNER JOIN dept_emp as de
 INNER JOIN titles as ti
 	ON(e.emp_no = ti.emp_no)
 	
+	
+---
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+Left outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
 
+	
